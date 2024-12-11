bb248a534616ff052964a325bdaa6e749540c1c3
fescaladag <fescaladag@uoc.edu>
2024-12-11 21:50:13 +0100

    Ej.2- Navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,13 +6,20 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 
 export class NavbarComponent {
-  
+
+  // Controla si el menú está abierto o cerrado
+  isMenuOpen: boolean = false;
+
+  // Método para alternar el estado del menú
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 
 
 }
